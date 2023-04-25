773f66ac3cb52d68e8f5dd6952ef2ffb01e856c0
Angela Gilhotra <angelagilhotra@gmail.com>
2023-04-25 18:43:04 -0300

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
+                Justttttt the basics - Everything you need to know to set up your
                 database and authentication.
               </div>
             </Link>
