0c8de88971c828056c2aa68035a661eb3b0c9254
Angela Gilhotra <angelagilhotra@gmail.com>
2023-04-26 00:14:10 -0300

    stop logging

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -2,15 +2,10 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/src/server/api/trpc";
 
-const databaseUrl = process.env.DATABASE_URL || "";
-const virtualHost = process.env.VIRTUAL_HOST || "";
-
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
-      console.log(`database url: ${databaseUrl}`)
-      console.log(`Virtual Host: ${virtualHost}`)
       return {
         greeting: `Hello ${input.text}`,
       };
